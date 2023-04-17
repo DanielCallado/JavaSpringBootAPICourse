93bcb6ab6160265dc8bedc715e277a3d8710a1ee
Daniel A. C. Rodrigues <daniel.almeidac@sonda.com>
2023-04-17 11:33:30 -0300

    Mudando para EntityManager o CRUD de clientes

--- src/main/java/br/com/danielcallado/VendasApplication.java
+++ src/main/java/br/com/danielcallado/VendasApplication.java
@@ -1,10 +1,54 @@
 package br.com.danielcallado;
 
+import br.com.danielcallado.domain.entity.Cliente;
+import br.com.danielcallado.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            System.out.println("Salvando Clientes");
+            clientes.salvar(new Cliente("Daniel"));
+            clientes.salvar(new Cliente("Beatriz"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando Clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " Atualizado");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando Clientes");
+            clientes.buscarPorNome("triz").forEach(System.out::println);
+
+            System.out.println("Deletando Clientes");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            if (todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
