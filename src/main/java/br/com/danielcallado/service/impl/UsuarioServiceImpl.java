b9522168ec55711dbd29a79e5210010c3e9baed9
Daniel de A. C. Rodrigues <daniel.almeida.rodrigues@outlook.com>
2023-05-02 21:54:12 -0300

    Conclusão UsuarioServiceImpl e controller de usuario

--- src/main/java/br/com/danielcallado/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/com/danielcallado/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,7 @@
 package br.com.danielcallado.service.impl;
 
+import br.com.danielcallado.domain.entity.Usuario;
+import br.com.danielcallado.domain.repository.Usuarios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,23 +10,35 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class UsuarioServiceImpl implements UserDetailsService {
 
     @Autowired
     private PasswordEncoder encoder;
 
+    @Autowired
+    private Usuarios usuarios;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!username.equals("daniel")){
-            throw new UsernameNotFoundException("Usuário não encontrado na base.");
-        }
+        Usuario usuario = usuarios.findByLogin(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("Usuário não encontrado na base de dados."));
 
+
+        String[] roles = usuario.isAdmin() ?
+                new String[]{"ADMIN", "USER"} : new String[] {"USER"};
         return User
                 .builder()
-                .username("daniel")
-                .password(encoder.encode("123"))
-                .roles("USER", "ADMIN")
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(roles)
                 .build();
+    }
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return usuarios.save(usuario);
     }
 }
